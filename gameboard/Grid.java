983fa935ff0a26b7f0af860ad6193e1477d74de3
exicutioner161 <exicutioner161@gmail.com>
2026-09-10 18:21:31 -0400

    Fleshed out Grid.java methods

--- gameboard/Grid.java
+++ gameboard/Grid.java
@@ -1,5 +1,30 @@
 package gameboard;
 
 public class Grid {
+    private final Cell[][] arr;
+
+    public Grid() {
+        arr = new Cell[10][10];
+        fillArray();
+    }
+
+    private void fillArray() {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                arr[i][j] = new Cell(i, j);
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                // TODO
+            }
+        }
+        return sb.toString();
+    }
 
 }
