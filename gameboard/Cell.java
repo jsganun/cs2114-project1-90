2076b3cc7ae4007d3e4fa2b19a628a1d2c9053cd
exicutioner161 <exicutioner161@gmail.com>
2026-09-10 18:06:20 -0400

    Created display symbols and toString()

--- gameboard/Cell.java
+++ gameboard/Cell.java
@@ -1,11 +1,16 @@
 package gameboard;
+
 public class Cell {
     private final int x;
     private final int y;
+    private static final char HIDDEN_SYMBOL = '-';
+    private static final char REVEALED_SYMBOL = '+';
+    private String displaySymbol;
 
     public Cell(int x, int y) {
         this.x = x;
         this.y = y;
+        this.displaySymbol = String.valueOf(HIDDEN_SYMBOL);
     }
 
     public int getX() {
@@ -14,5 +19,14 @@ public class Cell {
 
     public int getY() {
         return y;
+    }
+
+    public void reveal() {
+        displaySymbol = String.valueOf(REVEALED_SYMBOL);
+    }
+
+    @Override
+    public String toString() {
+        return displaySymbol;
     }
 }
