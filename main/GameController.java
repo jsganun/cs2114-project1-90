73321fd7d837d7be51441d980c9a93b78ca9c73f
exicutioner161 <brian4182008@gmail.com>
2026-09-17 18:29:16 -0400

    Created basic GameController skeleton

--- main/GameController.java
+++ main/GameController.java
@@ -1,6 +1,7 @@
 package main;
 
 import gameboard.*;
+import java.util.Scanner;
 
 /**
  * Entry point for the game application.
@@ -10,14 +11,36 @@ public class GameController {
         System.out.println(grid.toString());
     }
 
+    private static void printMenu(Grid grid, int currentPlayerMove) {
+        String menu = """
+                1. Shot
+                2. Print grid
+                3. Print log
+                """;
+        System.out.println("Current move: Player"
+                + currentPlayerMove + "\n" + menu);
+    }
+
+    private static void runGame(Scanner input) {
+        Grid grid = new Grid();
+        int currentPlayerMove = 1;
+        while (true) {
+            printGrid(grid);
+            printMenu(grid, currentPlayerMove);
+            currentPlayerMove = currentPlayerMove == 1 ? 2 : 1;
+        }
+    }
+
     /**
      * Starts the game application.
      *
      * @param args command-line arguments
      */
     public static void main(String[] args) {
-        // TODO
-        Grid grid = new Grid();
-        printGrid(grid);
+
+        try (Scanner input = new Scanner(System.in)) {
+            runGame(input);
+        }
+
     }
 }
