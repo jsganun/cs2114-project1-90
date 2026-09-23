b4a3ec0e49952cb1e42cad822cf6fcb68c32c88f
jsganun <jsganun@vt.edu>
2026-09-23 17:40:31 -0400

    Changed AuditSystem imports and implemented AuditSystem methods

--- main/AuditSystem.java
+++ main/AuditSystem.java
@@ -1,6 +1,7 @@
 package main;
 
-import gameboard.Shot;
+import gameboard.*;
+import ships.*;
 import java.util.ArrayList;
 
 public class AuditSystem {
@@ -10,27 +11,74 @@ public class AuditSystem {
         list = new ArrayList<>();
     }
 
-    public void recordShot(Shot shot) {
+    /**
+     * Records a shot in the audit log.
+     *
+     * @param shot the shot to record
+     */
+    public void recordShot(int player, Shot shot) {
         String shotConnectionMsg = (shot.getResult() == gameboard.ShotResult.Hit || shot.getResult() == gameboard.ShotResult.Sunk) ? "Hit" : "Missed";
-        list.add("Shot: " + shotConnectionMsg);
+        list.add("Shot by Player " + player + ": " + shotConnectionMsg);
     }
 
-    // public recordHit(shot: Shot): void
+    /**
+     * Records the start of the game in the audit log.
+     */
+    public void recordGameStart() {
+        list.add("Game started.");
+    }
 
-    // public recordMiss(shot: Shot): void
+    /**
+     * Records the end of the game in the audit log.
+     * 
+     * @param winningPlayer the player number (1 or 2) who won the game
+     * @param byForfeit true if the game ended by forfeit, false if it ended by all ships being sunk
+     */
+    public void recordGameEnd(int winningPlayer, boolean byForfeit) {
+        if (byForfeit) {
+            list.add("Game ended by forfeit. Winning player: " + winningPlayer);
+        }
+        else {
+            list.add("Game ended by all ships sunk. Winning player: " + winningPlayer);
+        }
+    }
 
-    // public recordSink(shot: Shot): void
+    /**
+     * Records the placement of a ship in the audit log.
+     *
+     * @param player the player number (1 or 2) who placed the ship
+     * @param ship the ship that was placed
+     */
+    public void recordShipPlacement(int player, Ship ship) {
+        Cell[] cells = ship.getCells();
+        StringBuilder cellPositions = new StringBuilder();
 
-    // public recordGameStart(): void
+        for (Cell cell : cells) {
+            cellPositions.append("(").append(cell.getRow()).append(", ").append(cell.getCol()).append(") ");
+        }
 
-    // public recordGameEnd(): void
+        list.add("Player " + player + " placed ship: " + ship.getName() + " of length " + ship.getLength() + " at positions: " + cellPositions.toString());
+    }
 
+    /**
+     * Records the advancement of a turn in the audit log.
+     */
+    public void recordTurnAdvance(int player) {
+        list.add("Turn advanced to Player " + player);
+    }
+
+    /**
+     * Prints the audit log.
+     */
     public void printLog() {
         for (int i = 0; i < list.size(); i++) {
             System.out.println(i + ". " + list.get(i));
         }
     }
 
+    /**
+     * Clears the audit log.
+     */
     public void clearLog() {
         list = new ArrayList<>();
     }
