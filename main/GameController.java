fdd765f83a049b835c76439debc9bbebfeccfecf
exicutioner161 <brian4182008@gmail.com>
2026-09-24 15:58:14 -0400

    Made separator a private static final variable

--- main/GameController.java
+++ main/GameController.java
@@ -12,6 +12,7 @@ public class GameController {
     private static final Random RANDOM = new Random();
     private static int player1ShipsSunk = 0;
     private static int player2ShipsSunk = 0;
+    private static final String SEPARATOR = "--------------------------";
 
     /**
      * Prints the game grid to standard output.
@@ -35,9 +36,8 @@ public class GameController {
                 3. Print menu
                 4. Print log
                 """;
-        String separator = "--------------------------";
-        System.out.println(separator + "\nCurrent move: Player "
-                + currentPlayerMove + "\n" + menu + separator);
+        System.out.println(SEPARATOR + "\nCurrent move: Player "
+                + currentPlayerMove + "\n" + menu + SEPARATOR);
     }
 
     /**
