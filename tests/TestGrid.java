9dfad046f3d6b304ace218b4d1334d9ec92fe7ad
jsganun <jsganun@vt.edu>
2026-09-23 22:09:58 -0400

    Built skeleton for TestGrid

--- tests/TestGrid.java
+++ tests/TestGrid.java
@@ -1,9 +1,13 @@
 package tests;
 
+import org.junit.Test;
+
 import gameboard.Grid;
 
 public class TestGrid {
-    public static void testToString() {
+
+    @Test 
+    public void testToString() {
         Grid grid = new Grid(10, 10);
         String expected = """
                   1 2 3 4 5 6 7 8 9 10
@@ -23,7 +27,37 @@ public class TestGrid {
         }
     }
 
-    public static void main(String[] args) {
-        testToString();
+    @Test 
+    public void testGetCell() {
+        
     }
+
+    @Test
+    public void testPlaceShip() {
+        
+    }
+
+    @Test
+    public void testAllShipsSunk() {
+
+    }
+
+    @Test 
+    public void testIsValidPlacement() {
+        
+    }
+
+    @Test 
+    public void testReceiveShot() {
+        
+    }
+
+    @Test
+    public void testTranslateCoords() {
+        
+    }
+
+    // public static void main(String[] args) {
+    //     testToString();
+    // }
 }
